8669424590d3094541574f94e29899bb65277e3c
reza <parvvaresh@gmail.com>
2024-08-11 15:06:24 +0330

    save result csv in iters

--- Transform/Classification/train_models.py
+++ Transform/Classification/train_models.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 from parameter_finder import classification_parameter_finder
@@ -11,7 +12,7 @@ from sklearn.exceptions import ConvergenceWarning
 warnings.filterwarnings("ignore", category = ConvergenceWarning)
 
 
-def train_models(x_data : dict,
+def train_models(X : np.array,
                  y : pd.DataFrame):
     
 
@@ -22,7 +23,7 @@ def train_models(x_data : dict,
 
     details_models = get_details_models()
     for detail_model in details_models:
-        X_train, X_test, y_train, y_test = train_test_split(x_data, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
         model, parameters = detail_model
 
@@ -36,12 +37,17 @@ def train_models(x_data : dict,
                                                 y_test)
                 
         print(f"--- ✅finish train <<{model}>> on data")
+
         results.append(_result)
+        results_df = pd.concat(results, ignore_index=True)
+        results_df.to_csv("./result/result.csv")
+
+
 
     print("✅finish train model")
 
     results = pd.concat(results, ignore_index=True)
 
-    results.to_csv("result.csv")
+    results.to_csv("./result/result.csv")
 
     print("             ✅save result in local path✅              ")
