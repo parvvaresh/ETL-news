90e4de9d06f5a03c7ec0c680d697f6e9d0da6659
reza <parvvaresh@gmail.com>
2024-08-11 14:25:49 +0330

    fix code

---
+++ Transform/Classification/train_models.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from parameter_finder import classification_parameter_finder
+from models.models import get_details_models
+
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+
+# Ignore ConvergenceWarning
+warnings.filterwarnings("ignore", category = ConvergenceWarning)
+
+
+def train_models(x_data : dict,
+                 y : pd.DataFrame):
+    
+
+    results = []
+
+
+    print("📌start train model ...")
+
+    details_models = get_details_models()
+    for detail_model in details_models:
+        X_train, X_test, y_train, y_test = train_test_split(x_data, y, test_size=0.2, random_state=42)
+
+        model, parameters = detail_model
+
+        print(f"--- 📌start train <<{model}>> on data")
+
+        _result = classification_parameter_finder(model,
+                                                parameters,
+                                                X_train,
+                                                y_train,
+                                                X_test,
+                                                y_test)
+                
+        print(f"--- ✅finish train <<{model}>> on data")
+        results.append(_result)
+
+    print("✅finish train model")
+
+    results = pd.concat(results, ignore_index=True)
+
+    results.to_csv("result.csv")
+
+    print("             ✅save result in local path✅              ")
