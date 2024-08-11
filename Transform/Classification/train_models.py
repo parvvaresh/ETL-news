99175b27002b81e836a9610167dc92e984691063
reza <parvvaresh@gmail.com>
2024-08-11 15:07:34 +0330

    rename var

--- Transform/Classification/train_models.py
+++ Transform/Classification/train_models.py
@@ -23,7 +23,7 @@ def train_models(X : np.array,
 
     details_models = get_details_models()
     for detail_model in details_models:
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         model, parameters = detail_model
 
